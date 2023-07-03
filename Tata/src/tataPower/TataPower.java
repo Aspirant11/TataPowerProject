1a7658c8f514db253f430dde99e88bb67eeaedf1
Abhishek <abhishekhedde21@gmail.com>
2023-07-03 11:51:51 +0000

    commit

---
+++ Tata/src/tataPower/TataPower.java
@@ -0,0 +1,10 @@
+package tataPower;
+
+public class TataPower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Welcome to Sts....");
+	}
+
+}
